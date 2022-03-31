36cb5db40b367a5a4faa6cdb32f2e3788306954a
digao08 <93328517+digao08@users.noreply.github.com>
2022-03-31 11:35:49 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,107 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+
+
+
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Criar Solo
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+ 	boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+
+
+if(keyDown(LEFT_ARROW)) {
+  helicopterSprite.velocityX= -5;
+  helicopterSprite.velocityY= 0;
+} 
+if(keyDown(RIGHT_ARROW)) {
+  helicopterSprite.velocityX= 5;
+  helicopterSprite.velocityY= 0;
+}
+
+
+var PAREDE=createSprite(3,120,10,100000);
+PAREDE.shapeColor="WHITE";//3
+
+var PAREDE2=createSprite(794,50,10,1000000);
+PAREDE2.shapeColor="WHITE";//4
+
+if(keyDown(DOWN_ARROW)){
+	packageSprite.velocityY=5;
+}
+packageSprite.bounce(boxBase)
+
+helicopterSprite.bounceOff(PAREDE);
+
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+  
+}
